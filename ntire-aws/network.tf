a8d18b06e0f74196f8ff9461ce3e563fefd01f36
abdulnadeem5150 <abdulnadeem5150@gmail.com>
2024-02-01 18:34:27 +0530

    SecurityGroup added

--- ntire-aws/network.tf
+++ ntire-aws/network.tf
@@ -14,20 +14,3 @@ resource "aws_subnet" "subnets" {
     Name = var.subnet_names[count.index]
   }
 }
-
-resource "aws_security_group" "open-ssh-http" {
-
-  name        = "open-ssh-http"
-  vpc_id      = aws_vpc.ntier-primary.id
-  depends_on  = [aws_vpc.ntier-primary]
-  description = "open-ssh-http"
-}
-
-resource "aws_vpc_security_group_ingress_rule" "sg_rules" {
-  security_group_id = aws_security_group.open-ssh-http.id
-  ip_protocol       = "tcp"
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 22
-  to_port           = 22
-
-}
