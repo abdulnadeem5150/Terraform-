c8b4b42fd43be58486418fb183d03c144f3e7b87
abdulnadeem5150 <abdulnadeem5150@gmail.com>
2024-02-09 17:01:45 +0530

    add

--- workshop/tfazure/provider.tf
+++ workshop/tfazure/provider.tf
@@ -5,12 +5,6 @@ terraform {
       version = "3.85.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "state"
-    storage_account_name = "qtterraformdec23"
-    container_name       = "states"
-    key                  = "workshop.tfstate"
-  }
 }
 
 provider "azurerm" {
